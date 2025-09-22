210410a1f76822b4a779b6375e371d6103f8d53d
Euller Braz <euller.braz99@gmail.com>
2025-09-22 16:16:59 -0300

    feat(server): add new dependencies and health-check route
    Added Fastify Swagger and Scalar Fastify API Reference as dependencies. Implemented a new health-check route using Fastify and Zod. Enhanced server setup with CORS support, error handling for schema validation errors, and API documentation.

--- server/src/infra/http/server.ts
+++ server/src/infra/http/server.ts
@@ -1,13 +1,50 @@
+import fastifyCors from '@fastify/cors';
+import { fastifySwagger } from '@fastify/swagger';
+import scalarUI from '@scalar/fastify-api-reference';
 import { fastify } from 'fastify';
-
-const app = fastify();
-
-app.get('/health', async () => {
-  return { status: 'ok' };
-});
+import {
+  hasZodFastifySchemaValidationErrors,
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod';
+import { healthCheckRoute } from './routes/health-check';
 
 const PORT = Number(process.env.PORT) || 3333;
 const HOST = process.env.HOST || '0.0.0.0';
+
+const app = fastify();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.setErrorHandler((error, _req, res) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return res.status(400).send({
+      message: 'Validation error.',
+      issues: error.validation,
+    });
+  }
+
+  console.error(error);
+
+  return res.status(500).send({ messsage: 'Internal server error.' });
+});
+
+app.register(fastifyCors, { origin: '*' });
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: { title: 'Brev.ly API', version: '1.0.0' },
+  },
+  transform: jsonSchemaTransform,
+});
+
+app.register(scalarUI, {
+  routePrefix: '/docs',
+});
+
+app.register(healthCheckRoute);
 
 app.listen({ port: PORT, host: HOST }).then(() => {
   console.log(`HTTP server running on http://${HOST}:${PORT}`);
